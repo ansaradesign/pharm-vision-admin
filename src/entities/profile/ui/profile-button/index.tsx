fafae90208d47fb815ae9e7e912f3923d9db545f
MariaTrof <lavandalarson@gmail.com>
2025-05-16 17:09:55 +0300

    profile button - вместо icon, Image

--- src/entities/profile/ui/profile-button/index.tsx
+++ src/entities/profile/ui/profile-button/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import Image from 'next/image';
+
 import UserBtn from './user-circle.svg';
 
 export const ProfileButton = () => {
   return (
     <button>
-      <img alt='profile img' src={UserBtn} />
+      <Image alt='Profile' height={50} quality={100} src={UserBtn} width={50} />
     </button>
   );
 };
